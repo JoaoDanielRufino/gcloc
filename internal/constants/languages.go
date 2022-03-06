48153039998b6324c8c27b226381f29e8aa0beda
JoaoDanielRufino <joaodaniel0405@gmail.com>
2022-03-06 12:04:11 -0300

    added more languages
    Signed-off-by: JoaoDanielRufino <joaodaniel0405@gmail.com>

--- internal/constants/languages.go
+++ internal/constants/languages.go
@@ -3,7 +3,15 @@ package constants
 import "github.com/JoaoDanielRufino/gcloc/pkg/gcloc/language"
 
 var Languages = language.Languages{
+	"Assembly": {
+		LineComments:      []string{"//", ";", "#", "@", "|", "!"},
+		MultiLineComments: [][]string{{"/*", "*/"}},
+	},
 	"C": {
+		LineComments:      []string{"//"},
+		MultiLineComments: [][]string{{"/*", "*/"}},
+	},
+	"C Header": {
 		LineComments:      []string{"//"},
 		MultiLineComments: [][]string{{"/*", "*/"}},
 	},
@@ -11,7 +19,23 @@ var Languages = language.Languages{
 		LineComments:      []string{"//"},
 		MultiLineComments: [][]string{{"/*", "*/"}},
 	},
+	"C++ Header": {
+		LineComments:      []string{"//"},
+		MultiLineComments: [][]string{{"/*", "*/"}},
+	},
 	"Golang": {
+		LineComments:      []string{"//"},
+		MultiLineComments: [][]string{{"/*", "*/"}},
+	},
+	"HTML": {
+		LineComments:      []string{},
+		MultiLineComments: [][]string{{"<!--", "-->"}},
+	},
+	"Haskell": {
+		LineComments:      []string{"--"},
+		MultiLineComments: [][]string{},
+	},
+	"Java": {
 		LineComments:      []string{"//"},
 		MultiLineComments: [][]string{{"/*", "*/"}},
 	},
@@ -19,11 +43,27 @@ var Languages = language.Languages{
 		LineComments:      []string{"//"},
 		MultiLineComments: [][]string{{"/*", "*/"}},
 	},
+	"PHP": {
+		LineComments:      []string{"//", "#"},
+		MultiLineComments: [][]string{{"/*", "*/"}},
+	},
+	"Prolog": {
+		LineComments:      []string{"%"},
+		MultiLineComments: [][]string{},
+	},
+	"Python": {
+		LineComments:      []string{"#"},
+		MultiLineComments: [][]string{{"\"\"\"", "\"\"\""}},
+	},
 	"TypeScript": {
 		LineComments:      []string{"//"},
 		MultiLineComments: [][]string{{"/*", "*/"}},
 	},
 	"Makefile": {
+		LineComments:      []string{"#"},
+		MultiLineComments: [][]string{},
+	},
+	"YAML": {
 		LineComments:      []string{"#"},
 		MultiLineComments: [][]string{},
 	},
