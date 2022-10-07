a6656e9ebd215283c1bf2ec4cf85e5911c678a99
ChandanChainani <chainanichan@gmail.com>
2022-10-07 13:53:18 +0530

    Add ruby code sample for test (#40)

---
+++ test/fixtures/code_samples/main.rb
@@ -0,0 +1,24 @@
+def segregate(a)
+  left, right = 0, a.length - 1
+  while left < right
+    # Increment left index while we see 0 at left
+    while a[left] == 0
+      left += 1
+    end
+
+    # Decrement right index while we see 1 at right
+    while a[right] == 1
+      right -= 1
+    end
+
+    # Exchange arr[left] and arr[right]
+    if left < right
+      a[left], a[right] = a[right], a[left]
+      left += 1
+      right -= 1
+    end
+  end
+  return a
+end
+
+p segregate([1,0,1,1,0,0]) # => [0, 0, 0, 1, 1, 1]
